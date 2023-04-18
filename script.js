16e2be694ab34c7e8693a50cb0e4c72816ae57df
Caleb Gardner <calebq23@gmail.com>
2023-04-18 18:27:29 -0500

    Sidebar

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function onLoad(){
 }
 
 var darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
+var sidebarExtended = false
 
 /**
  * @param {boolean} dark
@@ -39,6 +40,33 @@ function resizeContentSpace(){
     }
 }
 
+function sidebarExtend(){
+    var sidebar = document.getElementById("sidebar")
+    var cover = document.getElementById("cover")
+    if(sidebar == null || cover == null){
+        return
+    }
+    if(!sidebarExtended){
+        sidebarExtended = true
+        sidebar.animate([{left: sidebar.getAttribute("left")}, {left: (window.innerWidth - getTotalWidth("sidebar"))+"px"}], 300).onfinish = () =>
+            sidebar?.setAttribute("style", "left:" + (window.innerWidth - getTotalWidth("sidebar"))+"px")
+        cover.animate([{opacity: cover.getAttribute("opacity"), visibility: "visible"}, {opacity: "20%"}], 300).onfinish = () =>
+            cover?.setAttribute("style", "visibility: visible; opacity: 20%")
+    }else{
+        sidebarExtended = false;
+        sidebar.animate([{left: sidebar.getAttribute("left")}, {left: window.innerWidth + "px"}], 300).onfinish = () =>
+            sidebar?.setAttribute("style", "left:" + window.innerWidth + "px")
+        cover.animate([{opacity: cover.getAttribute("opacity")}, {opacity: "0%"}], 300).onfinish = () =>
+            cover?.setAttribute("style", "visibility: hidden; opacity: 0%")
+    }
+}
+
+function coverClick(){
+    if(sidebarExtended){
+        sidebarExtend()
+    }
+}
+
 /**
  * @param {string} id
  */
@@ -52,4 +80,19 @@ function getTotalHeight(id){
     height += parseInt(sty.marginTop)
     height += parseInt(sty.marginBottom)
     return height
+}
+
+/**
+ * @param {string} id
+ */
+function getTotalWidth(id){
+    let elem = document.getElementById(id)
+    if (elem == null){
+        return 0
+    }
+    var sty = window.getComputedStyle(elem)
+    var width = elem.clientWidth
+    width += parseInt(sty.marginLeft)
+    width += parseInt(sty.marginRight)
+    return width
 }
