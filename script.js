c64b3e2b3f9b83fe8d8c6ecf17e324bee482c82a
Caleb Gardner <calebq23@gmail.com>
2023-04-25 15:55:40 -0500

    Re-write of many parts to make it work correctly on mobile

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 // @ts-check
 
-function onLoad(){
-    setTheme(darkMode)
-    getPage()
-}
-
 var darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?? true
 var sidebarExtended = false
 
-/**
- * @param {boolean} dark
- */
-function setTheme(dark){
-    var styl = document.getElementById("styleLink")
-    var them = document.getElementById("theme")
-    if(them == null){
+function onLoad(){
+    if(!darkMode){
+        setTheme()
+    }
+    setToPath(window.location.pathname, false)
+    setSidebarWidth()
+    window.addEventListener("popstate", () => setToPath(window.location.pathname, false))
+}
+
+function setSidebarWidth(){
+    var sideContent = document.getElementById("sidebarContent")
+    if(sideContent == null){
         return
     }
-    if (dark){
+    sideContent.style.width = Math.min(window.innerWidth, 400).toString() + "px"
+}
+
+async function setTheme(){
+    var styl = document.getElementById("themeLink")
+    var them = document.getElementById("themeButton")
+    if(them == null || styl == null){
+        return
+    }
+    if (darkMode){
         styl?.setAttribute("href", "https://darkstorm.tech/dark-mode.css")
         them.innerText = "Light Mode"
     }else{
@@ -28,87 +37,87 @@ function setTheme(dark){
 
 function swapTheme(){
     darkMode = !darkMode
-    setTheme(darkMode)
+    setTheme()
 }
 
-function sidebarExtend(){
+async function expandSidebar(){
     var sidebar = document.getElementById("sidebar")
-    var cover = document.getElementById("cover")
+    var cover = document.getElementById("sidebarCover")
     if(sidebar == null || cover == null){
         return
     }
     if(!sidebarExtended){
         sidebarExtended = true
-        sidebar.animate([{left: sidebar.getAttribute("left")}, {left: "calc(100% - " + getTotalWidth("sidebar") + "px)"}], 300).onfinish = () =>
-            sidebar?.setAttribute("style", "left: calc(100% - " + getTotalWidth("sidebar") + "px)")
-        cover.animate([{opacity: cover.getAttribute("opacity"), visibility: "visible"}, {opacity: "20%"}], 300).onfinish = () =>
-            cover?.setAttribute("style", "visibility: visible; opacity: 20%")
+        sidebar.animate([{width: sidebar.style.width}, {width: "min(100%, 400px)"}], 300).onfinish = () =>{
+            if(sidebar != null) sidebar.style.width = "min(100%, 400px)"
+        }
+        cover.style.visibility = "visible"
+        cover.animate([{opacity: cover.style.opacity}, {opacity: "40%"}], 300).onfinish = () =>{
+            if(cover != null) cover.style.opacity = "40%"
+        }
     }else{
-        sidebarExtended = false;
-        sidebar.animate([{left: sidebar.getAttribute("left")}, {left: "100%"}], 300).onfinish = () =>
-            sidebar?.setAttribute("style", "left: 100%")
-        cover.animate([{opacity: cover.getAttribute("opacity")}, {opacity: "0%"}], 300).onfinish = () =>
-            cover?.setAttribute("style", "visibility: hidden; opacity: 0%")
-    }
-}
-
-function coverClick(){
-    if(sidebarExtended){
-        sidebarExtend()
-    }
-}
-
-/**
- * @param {string} id
- */
-function getTotalHeight(id){
-    let elem = document.getElementById(id)
-    if (elem == null){
-        return 0
-    }
-    var sty = window.getComputedStyle(elem)
-    var height = elem.clientHeight
-    height += parseInt(sty.marginTop)
-    height += parseInt(sty.marginBottom)
-    return height
-}
-
-/**
- * @param {string} id
- */
-function getTotalWidth(id){
-    let elem = document.getElementById(id)
-    if (elem == null){
-        return 0
-    }
-    var sty = window.getComputedStyle(elem)
-    var width = elem.clientWidth
-    width += parseInt(sty.marginLeft)
-    width += parseInt(sty.marginRight)
-    return width
-}
-
-function getPage(){
-    var content = document.getElementById("content")
-    if(content == null){
-        return
-    }
-    var loc = window.location.pathname
-    if(loc.startsWith("/")){
-        loc = loc.substring(1)
-    }
-    if(loc.endsWith("/")){
-        loc = loc.substring(0, loc.length-1)
-    }
-    if(loc == ""){
-        loc = "default"
-    }
-    window.fetch("https://api.darkstorm.tech/page/" + loc + "/?key=d5ca268e-5f83-4003-adfc-26b9e6bb47c9").then(async (resp) => {
-        if(resp.ok){
-            var txt = await resp.text()
-            if(content?.innerHTML != undefined){
-                content.innerHTML = txt
+        sidebarExtended = false
+        sidebar.animate([{width: sidebar.style.widows}, {width: "0px"}], 300).onfinish = () =>{
+            if(sidebar != null) sidebar.style.width = "0px"
+        }
+        cover.animate([{opacity: cover.style.opacity}, {opacity: "0%"}], 300).onfinish = () =>{
+            if(cover != null){
+                cover.style.opacity = "0%"
+                cover.style.visibility = "hidden"
             }
         }
+    }
+}
+
+/**
+* @param {string} path
+* @param {boolean} addToHistory
+*/
+async function setToPath(path, addToHistory = true){
+    if(sidebarExtended){
+        expandSidebar()
+    }
+    if(path.startsWith("/")){
+        path = path.substring(1)
+    }
+    if(path.endsWith("/")){
+        path = path.substring(0, path.length - 1)
+    }
+    if(path == ""){
+        path = "default"
+    }
+    setContent("https://api.darkstorm.tech/page/" + path + "/?key=d5ca268e-5f83-4003-adfc-26b9e6bb47c9", path, addToHistory)
+}
+
+/**
+* @param {string} url
+* @param {string} path
+* @param {boolean} addToHistory
+*/
+async function setContent(url, path, addToHistory = true){
+    if(addToHistory){
+        if(path == "default"){
+            path = ""
+        }
+        history.pushState(path, "", path)
+    }
+    window.fetch(url).then(async (resp) => {
+        var content = document.getElementById("content")
+        if(content == null){
+            return
+        }
+        content.innerHTML = await resp.text()
+        if(path == "resume") setupResumeSelector()
+    })
+}
+
+async function setupResumeSelector(){
+    var sel = document.getElementById("langSelect")
+    if(sel == null){
+        return
+    }
+    sel.addEventListener("change", () => {
+        // @ts-ignore
+        setContent("https://api.darkstorm.tech/page/resume/?key=d5ca268e-5f83-4003-adfc-26b9e6bb47c9&lang=" + sel.value, "resume", false)
     })
 }
