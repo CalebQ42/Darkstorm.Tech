2e5376226d14d54b6bceffd987123e54e3942b42
Caleb Gardner <calebq23@gmail.com>
2024-11-08 06:56:37 -0600

    Login screen styling

--- script.js
+++ script.js
@@ -1,84 +1,94 @@
 // @ts-check
+import "htmx";
 
 var darkMode =
-  window.matchMedia("(prefers-color-scheme: dark)").matches ?? true;
+	window.matchMedia("(prefers-color-scheme: dark)").matches ?? true;
 var sidebarExtended = false;
 
 function onLoad() {
-  if (!darkMode) {
-    setTheme();
-  }
-  setSidebarWidth();
-  // window.addEventListener("popstate", () =>
-  //   setToPath(window.location.pathname, false),
-  // );
+	if (!darkMode) {
+		setTheme();
+	}
+	setSidebarWidth();
+
+	// window.addEventListener("popstate", () =>
+	//   setToPath(window.location.pathname, false),
+	// );
 }
 
 function setSidebarWidth() {
-  var sideContent = document.getElementById("sidebarContent");
-  if (sideContent == null) {
-    return;
-  }
-  sideContent.style.width = Math.min(window.innerWidth, 400).toString() + "px";
+	var sideContent = document.getElementById("sidebarContent");
+	if (sideContent == null) {
+		return;
+	}
+	sideContent.style.width = Math.min(window.innerWidth, 400).toString() + "px";
 }
 
 async function setTheme() {
-  var styl = document.getElementById("themeLink");
-  var them = document.getElementById("themeButton");
-  if (them == null || styl == null) {
-    return;
-  }
-  if (darkMode) {
-    styl?.setAttribute("href", "https://darkstorm.tech/dark-mode.css");
-    them.innerText = "Light Mode";
-  } else {
-    styl?.setAttribute("href", "https://darkstorm.tech/light-mode.css");
-    them.innerText = "Dark Mode";
-  }
+	var styl = document.getElementById("themeLink");
+	var them = document.getElementById("themeButton");
+	if (them == null || styl == null) {
+		return;
+	}
+	if (darkMode) {
+		styl?.setAttribute("href", "https://darkstorm.tech/dark-mode.css");
+		them.innerText = "Light Mode";
+	} else {
+		styl?.setAttribute("href", "https://darkstorm.tech/light-mode.css");
+		them.innerText = "Dark Mode";
+	}
 }
 
 function swapTheme() {
-  darkMode = !darkMode;
-  setTheme();
+	darkMode = !darkMode;
+	setTheme();
 }
 
 async function expandSidebar() {
-  var sidebar = document.getElementById("sidebar");
-  var cover = document.getElementById("sidebarCover");
-  if (sidebar == null || cover == null) {
-    return;
-  }
-  if (!sidebarExtended) {
-    sidebarExtended = true;
-    sidebar.animate(
-      [{ width: sidebar.style.width }, { width: "min(100%, 400px)" }],
-      300,
-    ).onfinish = () => {
-      if (sidebar != null) sidebar.style.width = "min(100%, 400px)";
-    };
-    cover.style.visibility = "visible";
-    cover.animate(
-      [{ opacity: cover.style.opacity }, { opacity: "40%" }],
-      300,
-    ).onfinish = () => {
-      if (cover != null) cover.style.opacity = "40%";
-    };
-  } else {
-    sidebarExtended = false;
-    sidebar.animate(
-      [{ width: sidebar.style.widows }, { width: "0px" }],
-      300,
-    ).onfinish = () => {
-      if (sidebar != null) sidebar.style.width = "0px";
-    };
-    cover.animate(
-      [{ opacity: cover.style.opacity }, { opacity: "0%" }],
-      300,
-    ).onfinish = () => {
-      if (cover != null) {
-        cover.style.opacity = "0%";
-        cover.style.visibility = "hidden";
-      }
-    };
-  }
+	var sidebar = document.getElementById("sidebar");
+	var cover = document.getElementById("sidebarCover");
+	if (sidebar == null || cover == null) {
+		return;
+	}
+	if (!sidebarExtended) {
+		sidebarExtended = true;
+		sidebar.animate(
+			[{ width: sidebar.style.width }, { width: "min(100%, 400px)" }],
+			300,
+		).onfinish = () => {
+			if (sidebar != null) sidebar.style.width = "min(100%, 400px)";
+		};
+		cover.style.visibility = "visible";
+		cover.animate(
+			[{ opacity: cover.style.opacity }, { opacity: "40%" }],
+			300,
+		).onfinish = () => {
+			if (cover != null) cover.style.opacity = "40%";
+		};
+	} else {
+		sidebarExtended = false;
+		sidebar.animate(
+			[{ width: sidebar.style.widows }, { width: "0px" }],
+			300,
+		).onfinish = () => {
+			if (sidebar != null) sidebar.style.width = "0px";
+		};
+		cover.animate(
+			[{ opacity: cover.style.opacity }, { opacity: "0%" }],
+			300,
+		).onfinish = () => {
+			if (cover != null) {
+				cover.style.opacity = "0%";
+				cover.style.visibility = "hidden";
+			}
+		};
+	}
+}
+
+/**
+ * @param {SubmitEvent} e
+ */
+function login(e) {
+	e.preventDefault();
+	console.log(e);
 }
